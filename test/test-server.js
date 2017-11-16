a97344cc66b081c4a32cbd5978f1f9fa8297d959
rk-codes <ping.you.back@gmail.com>
2017-11-16 10:34:36 -0800

    code cleanup

--- test/test-server.js
+++ test/test-server.js
@@ -7,18 +7,17 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Blog API', function() {
-
 	before(function() {
-    	return runServer();
-  	});
-  	after(function() {
-    	return closeServer();
-  	});
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
 
-  	it('should list blog posts on GET', function() {
-  		return chai.request(app)
-  		  .get('/blog-posts')
-  		  .then(function(res) {
+  it('should list blog posts on GET', function() {
+  	return chai.request(app)
+  		.get('/blog-posts')
+  		.then(function(res) {
   			res.should.have.status(200);
   			res.should.be.json;
   			res.body.should.be.a('array');
@@ -31,52 +30,52 @@ describe('Blog API', function() {
   		});	
   	});
 
-  	it('should add blog post on POST', function() {
-  		const newItem = {
-        title : "New Title", 
-        content: "New Content ", 
-        author: "CK"
-      };
-  		return chai.request(app)
-  		  .post('/blog-posts')
-  		  .send(newItem)
-  		  .then(function(res) {
+  it('should add blog post on POST', function() {
+  	const newItem = {
+      title : "New Title", 
+      content: "New Content ", 
+      author: "CK"
+    };
+  	return chai.request(app)
+  		.post('/blog-posts')
+  		.send(newItem)
+  		.then(function(res) {
   			res.should.have.status(201);
   			res.should.be.json;
   			res.body.should.be.a('object');
   			res.body.should.include.keys('title', 'content', 'author');
   			res.body.id.should.not.be.null;
   		});
-  	});
+  });
 
-  	it('should delete blog post on DELETE', function() {
+  it('should delete blog post on DELETE', function() {
+  	return chai.request(app)
+  		.get('/blog-posts')
+  		.then(function(res) {
   		return chai.request(app)
-  		  .get('/blog-posts')
-  		  .then(function(res) {
-  			return chai.request(app)
-  				.delete(`/blog-posts/${res.body[0].id}`);
+  			.delete(`/blog-posts/${res.body[0].id}`);
   		})
   		.then(function(res) {
   			res.should.have.status(204);
   		});
-  	});
+  });
 
-  	it('should update blog post on PUT', function() {
-      const updateData = {
-        title : "New Title", 
-        content: "New Content ", 
-        author: "CKRK"
-      }
-      return chai.request(app)
-       .get('/blog-posts')
-       .then(function(res) {
-          updateData.id = res.body[0].id;
-          return chai.request(app)
-           .put(`/blog-posts/${updateData.id}`)
-           .send(updateData);
+  it('should update blog post on PUT', function() {
+    const updateData = {
+      title : "New Title", 
+      content: "New Content ", 
+      author: "CKRK"
+    }
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/blog-posts/${updateData.id}`)
+          .send(updateData);
        })
-       .then(function(res) {
-         res.should.have.status(204);
-       });
-  	});
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
 });
